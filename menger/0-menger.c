f3f5430ada4cad588e8892ae218c26e38bc47b3d
Abimael <icemask2612@gmail.com>
2025-03-23 11:36:30 -0400

    fixing betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -9,14 +9,14 @@
  */
 int is_blank(int x, int y)
 {
-    while (x > 0 || y > 0)
-    {
-        if (x % 3 == 1 && y % 3 == 1)
-            return (1);
-        x /= 3;
-        y /= 3;
-    }
-    return (0);
+	while (x > 0 || y > 0)
+	{
+		if (x % 3 == 1 && y % 3 == 1)
+			return (1);
+		x /= 3;
+		y /= 3;
+	}
+	return (0);
 }
 
 /**
@@ -25,22 +25,22 @@ int is_blank(int x, int y)
  */
 void menger(int level)
 {
-    int size, x, y;
+	int size, x, y;
 
-    if (level < 0)
-        return;
+	if (level < 0)
+		return;
 
-    size = pow(3, level);
+	size = pow(3, level);
 
-    for (y = 0; y < size; y++)
-    {
-        for (x = 0; x < size; x++)
-        {
-            if (is_blank(x, y))
-                putchar(' ');
-            else
-                putchar('#');
-        }
-        putchar('\n');
-    }
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			if (is_blank(x, y))
+				putchar(' ');
+			else
+				putchar('#');
+		}
+		putchar('\n');
+	}
 }
