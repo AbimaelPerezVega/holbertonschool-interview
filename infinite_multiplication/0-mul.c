2de0cdd1e9b7ae9b365c9f5591ba167cb0054653
Abimael <icemask2612@gmail.com>
2025-05-27 14:35:46 -0400

    infinite mult

---
+++ infinite_multiplication/0-mul.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point, multiplies two positive numbers.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: Always 0 (Success), or 98 on error.
+ */
+int main(int argc, char *argv[])
+{
+    char *num1, *num2;  // Strings to store the numbers
+    int len1, len2;      // Lengths of the numbers
+
+    // Validate number of arguments
+    if (argc != 3)
+    {
+        printf("Error\n");
+        return 98;
+    }
+
+    // Validate num1 and num2 contain only digits
+    num1 = argv[1];
+    num2 = argv[2];
+    len1 = strlen(num1);
+    len2 = strlen(num2);
+    for (int i = 0; i < len1; i++)
+    {
+        if (!isdigit(num1[i]))
+        {
+            printf("Error\n");
+            return 98;
+        }
+    }
+    for (int i = 0; i < len2; i++)
+    {
+        if (!isdigit(num2[i]))
+        {
+            printf("Error\n");
+            return 98;
+        }
+    }
+
+    // Perform multiplication logic here
+    // ...
+
+    // Print result
+    // printf("%s\n", result);
+
+    return 0;
+}
