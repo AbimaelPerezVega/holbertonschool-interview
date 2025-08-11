2f2b81b8d85d1ed7b3854b113fa5466ac2427b39
Abimael <icemask2612@gmail.com>
2025-08-11 10:58:26 -0400

    substring

--- substring/substring.c
+++ substring/substring.c
@@ -3,6 +3,50 @@
 #include "substring.h"
 
 /**
+ * match_at_index - checks if substring starting at index matches all words
+ * @s: the string to check
+ * @start: index in s to start checking
+ * @words: list of words
+ * @nb_words: number of words
+ * @word_len: length of each word
+ *
+ * Return: 1 if match found, 0 otherwise
+ */
+static int match_at_index(char const *s, int start,
+			  char const **words, int nb_words, int word_len)
+{
+	int *used, i, j, matched = 1;
+
+	used = calloc(nb_words, sizeof(int));
+	if (!used)
+		return (0);
+
+	for (i = 0; i < nb_words; i++)
+	{
+		const char *chunk = s + start + i * word_len;
+		int found = 0;
+
+		for (j = 0; j < nb_words; j++)
+		{
+			if (!used[j] && strncmp(chunk, words[j], word_len) == 0)
+			{
+				used[j] = 1;
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+		{
+			matched = 0;
+			break;
+		}
+	}
+
+	free(used);
+	return (matched);
+}
+
+/**
  * find_substring - finds all starting indices of substrings in `s`
  *                  that are concatenations of all words in `words`
  * @s: the main string to scan
@@ -12,77 +56,39 @@
  *
  * Return: allocated array of starting indices or NULL if none found
  */
-int *find_substring(char const *s, char const **words, int nb_words, int *n)
+int *find_substring(char const *s, char const **words,
+		    int nb_words, int *n)
 {
-    int s_len, word_len, total_len;
-    int *indices, count = 0;
-    int i, j;
-    char **temp_words;
-    int *found;
+	int s_len, word_len, total_len;
+	int *indices, count = 0, i;
 
-    *n = 0;
-    if (!s || !words || nb_words == 0)
-        return (NULL);
+	*n = 0;
+	if (!s || !words || nb_words == 0)
+		return (NULL);
 
-    s_len = strlen(s);
-    word_len = strlen(words[0]);
-    total_len = word_len * nb_words;
+	s_len = strlen(s);
+	word_len = strlen(words[0]);
+	total_len = word_len * nb_words;
 
-    if (s_len < total_len)
-        return (NULL);
+	if (s_len < total_len)
+		return (NULL);
 
-    indices = malloc(s_len * sizeof(int));
-    if (!indices)
-        return (NULL);
+	indices = malloc(s_len * sizeof(int));
+	if (!indices)
+		return (NULL);
 
-    /* Frequency array for words */
-    temp_words = malloc(nb_words * sizeof(char *));
-    if (!temp_words)
-    {
-        free(indices);
-        return (NULL);
-    }
+	for (i = 0; i <= s_len - total_len; i++)
+	{
+		if (match_at_index(s, i, words, nb_words, word_len))
+			indices[count++] = i;
+	}
 
-    for (i = 0; i <= s_len - total_len; i++)
-    {
-        found = calloc(nb_words, sizeof(int));
-        if (!found)
-        {
-            free(indices);
-            free(temp_words);
-            return (NULL);
-        }
-        for (j = 0; j < nb_words; j++)
-        {
-            temp_words[j] = (char *)s + i + j * word_len;
-            int k, matched = 0;
+	if (count == 0)
+	{
+		free(indices);
+		return (NULL);
+	}
 
-            for (k = 0; k < nb_words; k++)
-            {
-                if (!found[k] && strncmp(temp_words[j], words[k], word_len) == 0)
-                {
-                    found[k] = 1;
-                    matched = 1;
-                    break;
-                }
-            }
-            if (!matched)
-                break;
-        }
-        if (j == nb_words)
-            indices[count++] = i;
-
-        free(found);
-    }
-
-    free(temp_words);
-
-    if (count == 0)
-    {
-        free(indices);
-        return (NULL);
-    }
-
-    *n = count;
-    return (indices);
+	*n = count;
+	return (indices);
 }
